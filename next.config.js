464bbd769b40c453b0b63c179185f24a205fbb7f
Oluadeniran boluwatife Victor <157709372+Victorlegit@users.noreply.github.com>
2025-10-22 08:20:07 +0100

    Add initial Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizeCss: true,
+  },
+  compress: true,
+  poweredByHeader: false,
+}
+
+module.exports = nextConfig
